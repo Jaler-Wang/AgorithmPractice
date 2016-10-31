da648b268b303d809a5767adc7d679340e4d84f2
Jaler.wang <jalerw@gmail.com>
2016-10-31 22:02:59 +0800

    change user test

--- src/main/java/prefixSum/GetCarPairsNumber.java
+++ src/main/java/prefixSum/GetCarPairsNumber.java
@@ -7,6 +7,7 @@ package prefixSum;
  * <p>
  * 0 represents a car traveling east,
  * 1 represents a car traveling west.
+ * 
  * The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, is passing when P is traveling to the east and Q is traveling to the west.
  * source form: https://codility.com/programmers/lessons/5-prefix_sums/passing_cars/
  */

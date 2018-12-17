d7c5c6070fe3cf49c8f49ee97e37279d47a1d7aa
jianle.wang <jianle.wang@thomsonreuters.com>
2018-12-17 23:54:54 +0800

    update test case

--- src/main/java/string/BMTest.java
+++ src/main/java/string/BMTest.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 public class BMTest {
     @Test
     public void test(){
-        /*char[] source = "abcacabcbcbacabc".toCharArray();
-        char[] pattern = "cbacabc".toCharArray();*/
-        char[] source = "bacadaa".toCharArray();
-        char[] pattern = "aa".toCharArray();
+        char[] source = "abcacabcbcbacabc".toCharArray();
+        char[] pattern = "cbacabc".toCharArray();
+//        char[] source = "bacadaa".toCharArray();
+//        char[] pattern = "aa".toCharArray();
         BM bm = new BM();
         int index = bm.bm(source, pattern);
         System.out.println(index);
